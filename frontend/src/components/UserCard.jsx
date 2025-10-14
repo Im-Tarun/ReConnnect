980840b728888db5b9ea86560ad33a0e47c26910
Tarun Yaduwanshi <tarunyaduwanshi0@gmail.com>
2025-10-14 15:30:41 +0530

    message button fixed

--- frontend/src/components/UserCard.jsx
+++ frontend/src/components/UserCard.jsx
@@ -51,7 +51,7 @@ const UserCard = ({ user }) => {
 
     return (
         <div className="p-4 pt-6 w-full flex flex-col justify-between max-w-80 shadow border border-gray-200 rounded-md bg-white">
-            <div className="flex items-center flex-col gap-2 w-full cursor-pointer" onClick={() => navigate(`/profile/` + user._id)} >
+            <div className="flex items-center flex-col gap-2 w-full cursor-pointer" onClick={() => navigate(`/profile/${user?._id}`)} >
                 <img src={user?.profile_picture} alt="dp" className="size-20 rounded-full cursor-pointer object-cover" />
                 <div className="flex-1 cursor-pointer flex items-center flex-col" >
                     <p className="font-medium text-slate-700 ">{user?.full_name}</p>
@@ -79,12 +79,16 @@ const UserCard = ({ user }) => {
                     <span className="px-2" >{currentUser?.followings.includes(user?._id) ? "Following" : "Follow"}</span>
                 </button>
 
-                <button onClick={()=>handleFriendReq(user?._id)} className="flex flex-1 items-center justify-center bg-slate-200 hover:bg-slate-300 rounded-md py-3 px-5 active:scale-95 transition cursor-pointer bg-gradient-to-l from-indigo-500 to-purple-600 text-white">
-                    { !currentUser?.connections.includes(user?._id) ? <>
-                    <Plus className="w-4 h-4 text-white" /> 
-                    <span className="px-1">Friend</span></> : <>
+                <button onClick={()=>{
+                    currentUser?.connections.includes(user?._id) ? navigate(`/messages/${user?._id}`) :  handleFriendReq(user?._id)
+                }
+                    } className="flex flex-1 items-center justify-center bg-slate-200 hover:bg-slate-300 rounded-md py-3 px-5 active:scale-95 transition cursor-pointer bg-gradient-to-l from-indigo-500 to-purple-600 text-white">
+                    { currentUser?.connections.includes(user?._id) ? <>
                     <MessageCircle className="w-4 h-4 text-white"/>
                     <span className="px-2">Message</span>
+                    </> : <>
+                    <Plus className="w-4 h-4 text-white" /> 
+                    <span className="px-1">Friend</span>
                     </>}
                 </button> 
             </div>
