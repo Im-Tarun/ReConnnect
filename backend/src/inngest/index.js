aa1b8298bf9f8b9ddb0938be6941df602c2d802a
Tarun Yaduwanshi <tarunyaduwanshi0@gmail.com>
2025-12-19 01:50:41 +0530

    done story deletion in 24 hours

--- backend/src/inngest/index.js
+++ backend/src/inngest/index.js
@@ -149,8 +149,7 @@ export const deleteStory24Hour = inngest.createFunction(
   { event: "app/story-delete" },
   async ({ event, step }) => {
     const { storyId } = event.data;
-    // const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
-    const in24Hours = new Date(Date.now() + 60 * 1000);
+    const in24Hours = new Date(Date.now() + 24 * 60 * 60 * 1000);
 
     // Wait 24 hours
     await step.sleepUntil("wait-24-hours", in24Hours);
